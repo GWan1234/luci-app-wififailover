bbf715e25f81a9723c62e872344ee94c7424b03c
Vedeshkin Sergey <s.vedeshkin@zbrains.team>
2025-08-12 22:07:53 +0300

    на сегодня последний фикс

--- luci-app-wififailover/luasrc/model/cbi/wififailover/settings.lua
+++ luci-app-wififailover/luasrc/model/cbi/wififailover/settings.lua
@@ -1,13 +1,13 @@
 local uci = luci.model.uci.cursor()
 
-m = Map("wififailover", translate("WiFi Failover Settings"),
+local m = Map("wififailover", translate("WiFi Failover Settings"),
     translate("Configure WiFi failover parameters"))
 
 -- Общие настройки
-s = m:section(NamedSection, "settings", "wifi", translate("General Settings"))
+local s = m:section(NamedSection, "general", translate("General Settings"))
 s.addremove = false
 
-o = s:option(Value, "check_interval", translate("Check Interval (seconds)"))
+local o = s:option(Value, "check_interval", translate("Check Interval (seconds)"))
 o.datatype = "uinteger"
 o.default = "30"
 
@@ -15,34 +15,33 @@ o = s:option(Value, "ping_target", translate("Ping Target"))
 o.datatype = "host"
 o.default = "8.8.8.8"
 
--- Секция WiFi сетей с кастомным обработчиком
+-- Section for WiFi networks with a custom handler
 s = m:section(TypedSection, "wifi_network", translate("WiFi Networks"))
 s.template = "cbi/tblsection"
 s.addremove = true
-s.anonymous = false
+s.anonymous = true
 
--- Переопределяем создание новой секции
+-- Override the creation of a new section
 function s.create(self, section)
-    -- Создаем анонимную секцию
+    -- Create an anonymous section, UCI will assign a name
     local sid = uci:add("wififailover", "wifi_network")
-    uci:set("wififailover", sid, "type", "wifi_network")
     return sid
 end
 
--- Скрываем поле ввода ID
-id = s:option(DummyValue, ".name", translate("ID"))
+-- Hide the ID input field, but display the generated ID
+local id = s:option(DummyValue, ".name", translate("ID"))
 id.forcewrite = true
 function id.cfgvalue(self, section)
     return section
 end
 
--- Поля для конфигурации WiFi
-ssid = s:option(Value, "ssid", translate("SSID"))
-bssid = s:option(Value, "bssid", translate("BSSID"))
-key = s:option(Value, "key", translate("Password"))
+-- Fields for WiFi configuration
+local ssid = s:option(Value, "ssid", translate("SSID"))
+local bssid = s:option(Value, "bssid", translate("BSSID"))
+local key = s:option(Value, "key", translate("Password"))
 key.password = true
 
-encr = s:option(ListValue, "encryption", translate("Encryption"))
+local encr = s:option(ListValue, "encryption", translate("Encryption"))
 encr:value("none", "No Encryption")
 encr:value("psk", "WPA-PSK")
 encr:value("psk2", "WPA2-PSK")
